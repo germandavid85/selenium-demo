9179fa4563ab3cc43f184f544dba0080bc6c7955
German Potes <germandavid85@gmail.com>
2017-02-11 18:29:38 -0500

    Changes with asserts and other selectors

--- src/test/java/org/seleniumdemo/selenium/ProjectsTest.java
+++ src/test/java/org/seleniumdemo/selenium/ProjectsTest.java
@@ -3,21 +3,18 @@ package org.seleniumdemo.selenium;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
 
 public class ProjectsTest {
 
     private WebDriver driver;
     private String baseUrl;
-    private boolean acceptNextAlert = true;
-    private StringBuffer verificationErrors = new StringBuffer();
 
     @Before
     public void setUp() throws Exception {
@@ -29,55 +26,29 @@ public class ProjectsTest {
     @Test
     public void testSeleniumTestCase() throws Exception {
         driver.get(baseUrl);
-        driver.findElement(By.linkText("Projects")).click();
+        driver.findElement(By.id("menu_projects")).click();
         driver.findElement(By.linkText("Selenium WebDriver")).click();
-        driver.findElement(By.linkText("Projects")).click();
+        String webDriverTitle = driver.findElement(By.cssSelector("#mainContent h2")).getText();
+        assertEquals("Selenium WebDriver", webDriverTitle);
+
+        driver.findElement(By.id("menu_projects")).click();
         driver.findElement(By.linkText("Selenium Grid")).click();
-        driver.findElement(By.linkText("Projects")).click();
+        String gridTitle = driver.findElement(By.cssSelector("#mainContent h2")).getText();
+        assertEquals("Selenium Grid", gridTitle);
+
+        driver.findElement(By.id("menu_projects")).click();
         driver.findElement(By.linkText("Selenium IDE")).click();
-        driver.findElement(By.linkText("Projects")).click();
+        boolean isPluginLinkDisplayed = driver.findElement(By.linkText("Selenium IDE Plugins")).isDisplayed();
+        assertTrue(isPluginLinkDisplayed);
+
+        driver.findElement(By.id("menu_projects")).click();
         driver.findElement(By.linkText("Selenium Remote Control")).click();
-        driver.findElement(By.linkText("Projects")).click();
+        boolean isRCImageDisplayed = driver.findElement(By.cssSelector("img[alt='rc arch diagram']")).isDisplayed();
+        assertTrue(isRCImageDisplayed);
     }
 
     @After
     public void tearDown() throws Exception {
         driver.quit();
-        String verificationErrorString = verificationErrors.toString();
-        if (!"".equals(verificationErrorString)) {
-        }
-    }
-
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
-        }
     }
 }
